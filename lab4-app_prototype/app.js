7e4c8abbc92943fb0572da57e90d9abda0485786
Jadabyte <Jadabyte>
2020-03-28 15:55:44 +0100

    wrote checkTemp function to change the background and message based on the temperature outside, needs refining

--- lab4-app_prototype/app.js
+++ lab4-app_prototype/app.js
@@ -20,14 +20,30 @@ class App {
 
     getWeather(){
 
-        let url = `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/3416c702aea211069badc03171a6da3c/${this.lat},${this.lng}?units=si`;
+        let url = `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/e4f9c347eee0e309163dfee2b479555e/${this.lat},${this.lng}?units=si`;
         fetch(url).then(response => {
             return response.json();
         }).then(data => {
-            document.querySelector("#weather").innerHTML = data.currently.temperature;
+            document.querySelector("#weather").innerHTML = "It is currently " + Math.round(data.currently.temperature) + "°C";
+            let precipitation = data.currently.precipIntensity;
+            let temperature = Math.round(data.currently.temperature);
+            this.checkTemp();
         }).catch(err => {
             console.log(err);
         });
+    }
+
+    checkTemp(temperature, precipitation){
+        let message = document.querySelector("#message");
+        let background = document.querySelector("main");
+        if(temperature < 20 || precipitation > 0){
+            message.innerHTML = "Let's stay inside and play some Animal Crossing New Leaf";
+            background.style.background = "url('images/AnimalCrossing.jpg')";
+        }
+        else if(temperature > 20){
+            message.innerHTML = "Let's go out and play some Pokemon Go";
+            background.style.background = "url('images/PokemonGo.jpg')";
+        }
     }
 
     errorLocation(err){
