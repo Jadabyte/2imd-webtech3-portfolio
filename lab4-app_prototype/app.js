52181ad2d93aac2d5c5ebff3908aa2d1393d48e5
Jadabyte <Jadabyte>
2020-03-28 20:39:16 +0100

    checkTemp function finished, added soToStorage so the API isn't called too often

--- lab4-app_prototype/app.js
+++ lab4-app_prototype/app.js
@@ -1,8 +1,23 @@
 class App {
     constructor(){
-        this.getLocation();
-        this.lat;
-        this.lng;
+        //check if there is stuff in local storage
+        //if not, stuff needs to be stored
+        //the counter needs to be updated
+
+        //this makes sure that the api isn't being called too often
+        let minutes = 5;
+        let saved = localStorage.getItem('saved');
+        if(saved && (new Date()).getTime() - saved > minutes * 1000){
+            localStorage.clear()
+            this.getLocation();
+            this.lat;
+            this.lng;
+        }
+        else if(saved == null){
+            this.getLocation();
+            this.lat;
+            this.lng;
+        }
     }
 
     getLocation(){
@@ -19,31 +34,40 @@ class App {
     }
 
     getWeather(){
-
         let url = `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/e4f9c347eee0e309163dfee2b479555e/${this.lat},${this.lng}?units=si`;
         fetch(url).then(response => {
             return response.json();
         }).then(data => {
             document.querySelector("#weather").innerHTML = "It is currently " + Math.round(data.currently.temperature) + "°C";
+            let summary = data.currently.summary.toLowerCase();
             let precipitation = data.currently.precipIntensity;
             let temperature = Math.round(data.currently.temperature);
-            this.checkTemp();
+            this.checkTemp(temperature, precipitation, summary);
+            this.saveToStorage(temperature, precipitation, summary);
         }).catch(err => {
             console.log(err);
         });
     }
 
-    checkTemp(temperature, precipitation){
+    checkTemp(temperature, precipitation, summary){
         let message = document.querySelector("#message");
         let background = document.querySelector("main");
-        if(temperature < 20 || precipitation > 0){
-            message.innerHTML = "Let's stay inside and play some Animal Crossing New Leaf";
-            background.style.background = "url('images/AnimalCrossing.jpg')";
+        if(temperature < 10){
+            message.innerHTML = "Let's stay inside and play some Animal Crossing";
+            background.style.backgroundImage = "url('images/AnimalCrossing.jpg')";
+            background.style.backgroundPositionX = "-525px";
         }
-        else if(temperature > 20){
-            message.innerHTML = "Let's go out and play some Pokemon Go";
-            background.style.background = "url('images/PokemonGo.jpg')";
+        else if(precipitation > 0){
+            message.innerHTML = "It's " + summary + " right now, let's go out and play some Pokemon Go";
+            background.style.backgroundImage = "url('images/PokemonGo.jpg')";
+            background.style.backgroundPositionX = "-525px";
         }
+    }
+    saveToStorage(temperature, precipitation, summary){
+        localStorage.setItem("temperature", JSON.stringify(temperature));
+        localStorage.setItem("precipitation", JSON.stringify(precipitation));
+        localStorage.setItem("summary", JSON.stringify(summary));
+        localStorage.setItem('saved', new Date().getTime());
     }
 
     errorLocation(err){
